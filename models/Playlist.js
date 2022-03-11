f54190932d92ee7ddd20e02aa7e730d575a60fc2
Amy Shafer <amy.u.shafer@gmail.com>
2022-03-10 20:26:31 -0500

    user playlist and song models

---
+++ models/Playlist.js
@@ -0,0 +1,36 @@
+const { Model, Song } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Playlist extends Model {
+
+}
+
+Playlist.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+  },
+  {
+    sequelize, 
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'playlist',
+  }
+);
+
+module.exports = Playlist;
