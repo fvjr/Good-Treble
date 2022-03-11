3d203e041a046d33eac84f4dc36c905643433084
Fernando Vasquez <vasquezjrf@gmail.com>
2022-03-10 21:26:51 -0500

    updated models and index

--- models/index.js
+++ models/index.js
@@ -3,23 +3,31 @@ const Playlist = require('./Playlist');
 const Song = require('./Song');
 const Event = require('./Event');
 
-
 User.hasMany(Playlist, {
   foreignKey: 'user_id',
-  onDelete: 'CASCADE'
+  onDelete: 'CASCADE',
 });
 
 Playlist.belongsTo(User, {
-  foreignKey: 'user_id'
+  foreignKey: 'user_id',
+});
+
+Playlist.hasMany(Song, {
+  foreignKey: 'playlist_id',
+  onDelete: 'CASCADE',
 });
 
 Song.belongsTo(Playlist, {
-  foreignKey: 'user_id'
+  foreignKey: 'playlist_id',
+});
+
+User.hasMany(Event, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
 });
 
 Event.belongsTo(User, {
-  foreignKey: 'user_id'
+  foreignKey: 'user_id',
 });
 
-module.exports = { User, Playlist, Song, Event};
-
+module.exports = { User, Playlist, Song, Event };
