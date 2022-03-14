8e1980212bc08ba5b32d75b144e90b95b51f959d
Fernando Vasquez <vasquezjrf@gmail.com>
2022-03-14 19:44:22 -0400

    created logic to allow existing users to log in

--- public/js/login.js
+++ public/js/login.js
@@ -1,16 +1,14 @@
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const newUserName = document.querySelector('#name-signup').value.trim();
-  const newUserEmail = document.querySelector('#email-signup').value.trim();
-  const newUserPassword = document
-    .querySelector('#password-signup')
-    .value.trim();
+  const name = document.querySelector('#name-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
 
-  if (newUserName && newUserEmail && newUserPassword) {
+  if (name && email && password) {
     const response = await fetch('/api/users', {
       method: 'POST',
-      body: JSON.stringify({ newUserName, newUserEmail, newUserPassword }),
+      body: JSON.stringify({ name, email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
@@ -22,6 +20,34 @@ const signupFormHandler = async (event) => {
   }
 };
 
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+  // Collect values from the login form
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
+    // Send a POST request to the API endpoint
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      // If successful, redirect the browser to the profile page
+      document.location.replace('/homepage');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
 document
   .querySelector('.signup-form')
   .addEventListener('submit', signupFormHandler);
+
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
