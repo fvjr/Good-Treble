9f52502c08ae22df3502bd557e18c6c4f94e5db7
Fernando Vasquez <vasquezjrf@gmail.com>
2022-05-02 19:36:23 -0400

    started react file structure and components

---
+++ looking-for-treble/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import PageContainer from "./components/PageContainer";
+import './App.css';
+import './styles/Style.css'
+
+const App = () => <PageContainer/>;
+
+export default App;
