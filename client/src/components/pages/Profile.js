d7c7eb97923f35d6b23bc8a00f2a836a50871d0f
Fernando Vasquez <vasquezjrf@gmail.com>
2022-05-05 19:30:29 -0400

    created artist list component

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -4,6 +4,46 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ListGroup from 'react-bootstrap/ListGroup';
+import ArtistList from '../ArtistList';
+
+const artists = [
+  {
+    SongName: 'Wannabe In L.A',
+    ArtistName: 'Eagles Of Death Metal',
+    AlbumImage:
+      'https://i.scdn.co/image/ab67616d00004851f375d1acfe9121a92b7d29bb',
+    ArtistImage:
+      'https://i.scdn.co/image/c5bda32bcaefd65b004fec303555900f8be53fec',
+    id: 1,
+  },
+  {
+    SongName: "Walk, Don't Run",
+    ArtistName: 'Herb Alpert & The Tijuana Brass',
+    AlbumImage:
+      'https://i.scdn.co/image/ab67616d000048513c893f419757910dca22bb74',
+    ArtistImage:
+      'https://i.scdn.co/image/2311c347d3afc41e4b1feece205908b16af31e24',
+    id: 2,
+  },
+  {
+    SongName: 'Benzi Box',
+    ArtistName: 'DANGERDOOM',
+    AlbumImage:
+      'https://i.scdn.co/image/ab67616d000048518f4944a3d77dd680bde9fd10',
+    ArtistImage:
+      'https://i.scdn.co/image/0fe2fa0d70f965b2389cf6f658ee29950a9fbf17',
+    id: 3,
+  },
+  {
+    SongName: "Da' Dip",
+    ArtistName: 'Freak Nasty',
+    AlbumImage:
+      'https://i.scdn.co/image/ab67616d000048514831c1c2246ca4fd88f39f43',
+    ArtistImage:
+      'https://i.scdn.co/image/ab6761610000f17884871c3351c73543e3ad9dc5',
+    id: 4,
+  },
+];
 
 const styles = {
   artistDisplay: {
@@ -30,21 +70,7 @@ function Profile() {
             </a>
           </header>
         </Row>
-        <Row>Favorite Artists</Row>
-        <ListGroup horizontal>
-          <ListGroup.Item style={styles.artistDisplay}>Artist 1</ListGroup.Item>
-          <ListGroup.Item style={styles.artistDisplay}>Artist 2</ListGroup.Item>
-          <ListGroup.Item style={styles.artistDisplay}>Artist 3</ListGroup.Item>
-          <ListGroup.Item style={styles.artistDisplay}>Artist 4</ListGroup.Item>
-          <ListGroup.Item style={styles.artistDisplay}>Artist 5</ListGroup.Item>
-          <ListGroup.Item style={styles.artistDisplay}>Artist 6</ListGroup.Item>
-          <ListGroup.Item style={styles.artistDisplay}>Artist 7</ListGroup.Item>
-          <ListGroup.Item style={styles.artistDisplay}>Artist 8</ListGroup.Item>
-          <ListGroup.Item style={styles.artistDisplay}>Artist 9</ListGroup.Item>
-          <ListGroup.Item style={styles.artistDisplay}>
-            Artist 10
-          </ListGroup.Item>
-        </ListGroup>
+        <ArtistList artists={artists} />
         <Row>Favorite Songs</Row>
       </Container>
     </div>
