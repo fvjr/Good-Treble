5019c68cb07c8d0792834d7bcc3d03b0ec4d3d15
Fernando Vasquez <vasquezjrf@gmail.com>
2022-05-05 20:42:27 -0400

    updates to artistlist and favoritesongs components

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import '../../styles/style.css';
 
+const styles = {
+  aboutMe: {
+    fontSize: '1.2rem',
+    color: 'white',
+    padding: '15px',
+  },
+};
+
 export default function About() {
   return (
     <div className="container-fluid about-page my-2">
-      <p>
+      <p style={styles.aboutMe}>
         Good Treble has one goal. We want to help users find events for artists
         that they love, and make friends with other people who love those same
         artists and events. As music lovers, we know how rewarding it can be to
