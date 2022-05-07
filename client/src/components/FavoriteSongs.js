cc8d12f5fc8625230f2a445f24001dee89865c9e
Fernando Vasquez <vasquezjrf@gmail.com>
2022-05-06 21:59:10 -0400

    updated artistList component

--- client/src/components/FavoriteSongs.js
+++ client/src/components/FavoriteSongs.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ListGroupItem from 'react-bootstrap/esm/ListGroupItem';
-import {ListGroup,Col,Button,Row} from 'react-bootstrap'
-
+import { ListGroup, Col, Button, Row } from 'react-bootstrap';
 
 function FavoriteSongs(props) {
   return (
@@ -12,33 +11,35 @@ function FavoriteSongs(props) {
     //       <img src={song.AlbumImage} width="100" height="50" alt="song " />
     //     </li>
     //   ))} vuvfu
-    // </ul> 
+    // </ul>
     <ListGroup as="ol" numbered>
       {props.songs.map((song) => (
-  <ListGroup.Item 
-    as="li"
-    className="d-flex justify-content-between align-items-start"
-  > 
-    <div className="ms-2 me-auto">
-      <Row >
-      <Col md = {3}>
-    <img src={song.AlbumImage}  alt="song" />
-    </Col>
-    <Col md= {8} >
-      <div className="fw-bold" key={song.id}>{song.SongName}</div>
-      {song.ArtistName}
-      </Col>
-   
-    <Col md = {1}>
-     <Button bg="primary" pill>
-       14
-     </Button>
-     </Col>
-        </Row>
-        </div>
-  </ListGroup.Item>
-  ))}
-  </ListGroup>
+        <ListGroup.Item
+          as="li"
+          className="d-flex justify-content-between align-items-start"
+        >
+          <div className="ms-2 me-auto">
+            <Row>
+              <Col md={3}>
+                <img src={song.AlbumImage} alt="song" />
+              </Col>
+              <Col md={8}>
+                <div className="fw-bold" key={song.id}>
+                  {song.SongName}
+                </div>
+                {song.ArtistName}
+              </Col>
+
+              <Col md={1}>
+                <Button bg="primary" pill>
+                  14
+                </Button>
+              </Col>
+            </Row>
+          </div>
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
   );
 }
 
