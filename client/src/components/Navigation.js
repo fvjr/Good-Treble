03cb923b2392627635df0906757d5b65d0d62726
Amy Shafer <amy.u.shafer@gmail.com>
2022-05-09 20:24:13 -0400

    navbar changes

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -3,71 +3,88 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 
-function Navigation({ currentPage, handlePageChange }) {
-  return (
-    <div className="container-fluid mt-1">
-      <nav className="navbar navbar-expand-lg navbar-dark bg-transparent d-flex flex-row">
-        <a className="navbar-brand" href="/login">
-          <img
-            src="/Assets/clefArt.png"
-            width="30"
-            height="50"
-            alt="treble clef"
-          />
-        </a>
-        <div className="navbar-nav" id="navbarNav"></div>
-        <div className="mx-auto">
-          <a
-            className="navbar-logo"
-            href="#about"
-            onClick={() => handlePageChange('About')}
-          >
-            <img
-              src="/Assets/goodTrebleLogo.png"
-              width="135"
-              height="95"
-              alt="Good Treble Logo"
-            />
-          </a>
-        </div>
-        <div className="d-flex flex-row-reverse">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                href="/#about"
-                onClick={() => handlePageChange('About')}
-              >
-                Why GOOD TREBLE
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                href="/#profile"
-                onClick={() => handlePageChange('Profile')}
-              >
-                Profile
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                href="#login"
-                onClick={() => handlePageChange('Login')}
-              >
-                Login
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                ▶
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
+function Navigation({
+  currentPage,
+  handlePageChange
+}) {
+  return ( <
+    div className = "container-fluid mt-1" >
+    <
+    nav className = "navbar navbar-expand-lg navbar-dark bg-transparent d-flex flex-row" >
+    <
+    a className = "navbar-brand"
+    href = "/login" >
+    <
+    img src = "/Assets/clefArt.png"
+    width = "30"
+    height = "50"
+    alt = "treble clef" /
+    >
+    <
+    /a> <
+    div className = "navbar-nav"
+    id = "navbarNav" > < /div> <
+    div className = "mx-auto" >
+    <
+    a className = "navbar-logo"
+    href = "#about"
+    onClick = {
+      () => handlePageChange('About')
+    } >
+    <
+    img src = "/Assets/goodTrebleLogo.png"
+    width = "135"
+    height = "95"
+    alt = "Good Treble Logo" /
+    >
+    <
+    /a> <
+    /div> <
+    div className = "d-flex flex-row-reverse" >
+    <
+    ul className = "navbar-nav" >
+    <
+    li className = "nav-item" >
+    <
+    a className = "nav-link"
+    href = "/#about"
+    onClick = {
+      () => handlePageChange('About')
+    } >
+    Why GOOD TREBLE <
+    /a> <
+    /li> <
+    li className = "nav-item" >
+    <
+    a className = "nav-link"
+    href = "/#profile"
+    onClick = {
+      () => handlePageChange('Profile')
+    } >
+    Profile <
+    /a> <
+    /li> <
+    li className = "nav-item" >
+    <
+    a className = "nav-link"
+    href = "#login"
+    onClick = {
+      () => handlePageChange('Login')
+    } >
+    Login <
+    /a> <
+    /li> <
+    li className = "nav-item" >
+    <
+    a className = "nav-link"
+    href = "#" > ▶
+    <
+    /a> <
+    /li> <
+    /ul> <
+    /div> <
+    /nav> <
+    /div>
   );
 }
 export default Navigation;
