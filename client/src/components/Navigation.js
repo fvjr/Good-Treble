26925de2db87b0a1425af38bf5dd26a53f4b5bc9
Fernando Vasquez <vasquezjrf@gmail.com>
2022-05-11 22:39:45 -0400

    developed navigation and jumbotron components

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -32,17 +32,88 @@ const styles = {
     textAlign: 'center',
     fontWeight: 'bold',
   },
+  navBar: {
+    color: 'red',
+    backgroundColor: 'black',
+  },
 };
+
+// function Navigation() {
+//   return (
+//     <div style={styles.image}>
+//       <div className="container-fluid mt-1">
+//         <nav
+//           style={styles.image}
+//           className="navbar navbar-expand-lg navbar-dark bg-transparent d-flex flex-row"
+//         >
+//           <a className="navbar-brand" href="/login">
+//             <img
+//               src="/Assets/clefArt.png"
+//               width="30"
+//               height="50"
+//               alt="treble clef"
+//             />
+//           </a>
+//           <div className="navbar-nav" id="navbarNav"></div>
+//           <div className="mx-auto">
+//             <Link className="nav-link navbar-logo" to="/about">
+//               <img
+//                 src="/Assets/goodTrebleLogo.png"
+//                 width="135"
+//                 height="95"
+//                 alt="Good Treble Logo"
+//               />
+//             </Link>
+//           </div>
+//           <div className="d-flex flex-row-reverse">
+//             <ul className="navbar-nav">
+//               <li className="nav-item">
+//                 <Link className="nav-link" to="/about">
+//                   {' '}
+//                   Why GOOD TREBLE
+//                 </Link>
+//               </li>
+//               <li className="nav-item">
+//                 <Link className="nav-link" to="/eventsPage">
+//                   {' '}
+//                   Events Page
+//                 </Link>
+//               </li>
+//               <li className="nav-item">
+//                 <Link className="nav-link" to="/profile">
+//                   Profile
+//                 </Link>
+//               </li>
+//               <li className="nav-item">
+//                 <Link className="nav-link" to="/login">
+//                   Login
+//                 </Link>
+//               </li>
+//               <li className="nav-item">
+//                 <a
+//                   className="nav-link"
+//                   href="https://open.spotify.com/playlist/36mXEhFFU2cPrqcKmfoK0M?si=13590d54ca614540"
+//                 >
+//                   ▶
+//                 </a>
+//               </li>
+//             </ul>
+//           </div>
+//         </nav>
+//       </div>
+//     </div>
+//   );
+// }
 
 function Navigation() {
   return (
     <div style={styles.image}>
-      <div className="container-fluid mt-1">
+      <div>
         <nav
           style={styles.image}
           className="navbar navbar-expand-lg navbar-dark bg-transparent d-flex flex-row"
         >
-          <a className="navbar-brand" href="/login">
+          <a href="/login">
             <img
               src="/Assets/clefArt.png"
               width="30"
@@ -50,8 +121,8 @@ function Navigation() {
               alt="treble clef"
             />
           </a>
-          <div className="navbar-nav" id="navbarNav"></div>
-          <div className="mx-auto">
+          <div id="navbarNav"></div>
+          <div>
             <Link className="nav-link navbar-logo" to="/about">
               <img
                 src="/Assets/goodTrebleLogo.png"
@@ -61,7 +132,7 @@ function Navigation() {
               />
             </Link>
           </div>
-          <div className="d-flex flex-row-reverse">
+          <div>
             <ul className="navbar-nav">
               <li className="nav-item">
                 <Link className="nav-link" to="/about">
--- client/src/components/UserAvatar.js
+++ client/src/components/UserAvatar.js
@@ -1,6 +1,4 @@
-import React, {
-  useState
-} from 'react';
+import React, { useState } from 'react';
 import Image from 'react-bootstrap/Image';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -9,50 +7,67 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import { Link } from 'react-router-dom';
 
 const styles = {
+  title: {
+    padding: 20,
+    borderStyle: 'solid',
+    borderWdith: 15,
+    borderColor: 'black',
+    textAlign: 'center',
+  },
+  card: {
+    padding: 20,
+    borderStyle: 'solid',
+    borderWdith: 15,
+    borderColor: 'black',
+    textAlign: 'center',
+  },
+  image: {
+    textAlign: 'center',
+    display: 'block',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 'auto',
+    width: '100%',
+  },
+  artistName: {
+    padding: 20,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  navBar: {
+    color: 'red',
+  },
   profileHeader: {
     padding: 5,
     background: 'white',
     borderStyle: 'solid',
     borderWigth: 15,
     borderColor: 'black',
-    textAlign: 'left',
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 'auto',
+    width: '100%',
   },
 };
-
-// function UserAvatar(props) {
-//   return (
-//     <div style={styles.card}>
-//       <Row>
-//         <Col lg={true}>
-//           <Image
-//             fluid="true"
-//             roundedCircle="true"
-//             src="https://i.scdn.co/image/ab67616d000048518f4944a3d77dd680bde9fd10"
-//           ></Image>
-//         </Col>
-//         <Col lg={true}>
-//           <h2>USER NAME HERE</h2>
-//         </Col>
-//       </Row>
-//     </div>
-//   );
-// }
 
 function UserAvatar(props) {
   return (
     <Row style={styles.profileHeader}>
       <Col lg={true}>
-        <Image
-          fluid="true"
-          roundedCircle="true"
-          src="https://i.scdn.co/image/ab67616d000048518f4944a3d77dd680bde9fd10"
-        ></Image>
-      </Col>
+        <div>
+          <Image
+            fluid="true"
+            roundedCircle="true"
+            src="https://i.scdn.co/image/ab67616d000048518f4944a3d77dd680bde9fd10"
+          ></Image>
+          <Col>
+            <h2>USER NAME HERE</h2>
+          </Col>
+        </div>
+      </Col>{' '}
       <Col>
-        <h2>Community</h2>
-      </Col>
-      <Col>
-        <h2>USER NAME HERE</h2>
+        <h2>Community Page</h2>
       </Col>
       <Col>
         <Link to="/myEvents">
@@ -60,7 +75,9 @@ function UserAvatar(props) {
         </Link>
       </Col>
       <Col>
-        <h2>Events</h2>
+        <a href="https://www.savethemusic.org/">
+          <h2>SaveTheMusic Foundation</h2>
+        </a>
       </Col>
     </Row>
   );
