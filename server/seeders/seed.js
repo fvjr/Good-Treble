445c2654c289f128da15ddb49bd40e617d663e57
Fernando Vasquez <vasquezjrf@gmail.com>
2022-05-12 21:30:11 -0400

    further development of signupform

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,16 +1,29 @@
-const db = require('../config/connection');
+const sequelize = require('../config/connection');
 const { User } = require('../models');
-const userSeeds = require('./userSeeds.json');
+const userData = require('./userSeeds.json');
 
-db.sync('open', async () => {
-  try {
-    await User.deleteMany({});
-    await User.create(userSeeds);
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
+// sequelize.sync('open', async () => {
+//   try {
+//     await User.deleteMany({});
+//     await User.create(userSeeds);
+//   } catch (err) {
+//     console.error(err);
+//     process.exit(1);
+//   }
 
-  console.log('User information seeded.');
+//   console.log('User information seeded.');
+//   process.exit(0);
+// });
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
   process.exit(0);
-});
+};
+
+seedDatabase();
