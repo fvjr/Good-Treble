21e72c42bf63bda4bea9005422c716b3962814b6
Fernando Vasquez <vasquezjrf@gmail.com>
2022-05-12 20:58:58 -0400

    updates to login

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,3 +1,16 @@
-const db = rquire('../config/connection');
+const db = require('../config/connection');
 const { User } = require('../models');
 const userSeeds = require('./userSeeds.json');
+
+db.sync('open', async () => {
+  try {
+    await User.deleteMany({});
+    await User.create(userSeeds);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  console.log('User information seeded.');
+  process.exit(0);
+});
